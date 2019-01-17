9d68f11f2a9704026662cea563fc6b6aac330552
James Mwangi <james_mwangi@aol.com>
2019-01-17 09:54:04 +0300

    add date count and modify quote-description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';;
 import {FormsModule} from '@angular/forms';
+import { DateCountPipe } from './date-count.pipe';
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuoteFormComponent,
     QuoteDetailsComponent,
+    DateCountPipe,
   ],
   imports: [
     BrowserModule,
